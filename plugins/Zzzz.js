d774e214bdc88c5166db88d9200f374bca89f47f
RanzzNation <116924759+RanzzNation@users.noreply.github.com>
2022-10-29 11:48:59 +0700

    Update Zzzz.js

--- plugins/Zzzz.js
+++ plugins/Zzzz.js
@@ -7,13 +7,13 @@ let esce = `Hello kak ${ye} Lagi Nyari Sc Ya?
 💌 CARI AJA DIBAWAH INI
 
 📮 YOU TUBE :
-youtube.com/c/hokenbeusz
+https://youtube.com/c/ahmadstoreeee
 
 📮 GITHUB :
-github.com/krizynofc/forynfour-MDV1
+-
 
 📮 JANGAN LUPA MAMPIR :
-https://lynk.id/kri.com`
+-`
 
 await conn.sendBut(m.chat, esce, '📮 DONT FORGET TO FOLLOW MY ACCOUNT', 'Thanks', '.menu', m) 
 }
