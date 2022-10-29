503c78c8c828a6d277613af73a4fe4082546d174
RanzzNation <116924759+RanzzNation@users.noreply.github.com>
2022-10-29 12:23:38 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -28,20 +28,20 @@ VERSION:3.0
 N:;;;
 FN: ᯤ ᴋʀɪᴢʏɴ ᴏꜰᴄ
 item.ORG: ᴅᴇᴠᴇʟᴏᴩᴇʀ ʙᴏᴛ
-item1.TEL;waid=62895327934887:62895327934887@s.whatsapp.net
+item1.TEL;waid=6289697155781:6289697155781@s.whatsapp.net
 item1.X-ABLabel: Nomor Owner Bot
-item2.EMAIL;type=INTERNET: mhdfakri14@gmail.com
+item2.EMAIL;type=INTERNET: storeahmad362@gmail.com
 item2.X-ABLabel:📫 Gmail
 item3.ADR:;;📍 Acehnese - Indonesian;;;;
 item3.X-ABADR:ac
 item3.X-ABLabel:📮 Location Owner
-item4.URL:http://youtube.com/c/hokenbeusz
+item4.URL:https://youtube.com/c/ahmadstoreeee
 item4.X-ABLabel:Youtube
-item5.URL:https//github.com/krizynofc
+item5.URL:-
 item5.X-ABLabel:Github
-item6.URL:https//instagram.com/mhdfakri_
+item6.URL:-
 item6.X-ABLabel:Instagram
-item7.URL:https://lynk.id/kri.com
+item7.URL:-
 item7.X-ABLabel:Website
 END:VCARD`
 const sentMsg  = await conn.sendMessage(
